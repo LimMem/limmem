8be2e1e61ac5e2fdc8a0c1e2cba34bdf90d564f6
qcl901028 <qcl901028@gmail.com>
2021-03-16 18:34:29 +0800

    add app

--- src/commands/app.ts
+++ src/commands/app.ts
@@ -1,14 +1,19 @@
-import { Command, flags } from '@oclif/command'
+import {Command, flags} from '@oclif/command'
+import { existsSync } from 'fs';
+import { join } from 'path';
 import Generator from '../generator'
-import { absSrcPath } from '../utils'
+import { cwd } from '../utils';
 
-export default class Charts extends Command {
+export default class App extends Command {
   static description = 'describe the command here'
 
   static examples = [
-    `$ limmem charts [NAME] [SUBNAME]`,
+    `$ limmem App [NAME] [SUBNAME]`,
   ]
-
+  static args = [
+    { name: 'name', require: true, description: '组件名称' },
+    { name: 'subName', require: false, description: '子组件名称' }
+  ];
   static flags = {
     help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
@@ -16,16 +21,12 @@ export default class Charts extends Command {
     // flag with no value (-f, --force)
     force: flags.boolean({char: 'f'}),
   }
-  static args = [
-    { name: 'name', require: true, description: '组件名称' },
-    { name: 'subName', require: false, description: '子组件名称' }
-  ];
 
   async run() {
-    const {args} = this.parse(Charts)
+    const { args } = this.parse(App);
     const { name, subName } = args;
-    const srcPath = absSrcPath();
-    if (!srcPath) {
+    const srcPath = join(`${cwd()}`, 'src/component');
+    if (!existsSync(srcPath)) {
       this.error('src目录不存在，请cd到正确的文件路径', { code: '-1' });
     }
     const gen = new Generator({
